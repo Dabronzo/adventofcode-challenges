eb51a45155f34d93f24d0b276f9311d319f0770f
Ayrton <ayrtonterceiro@gmail.com>
2021-12-03 13:27:45 +0100

    Final part of the day 2 challenge

--- day-two.js
+++ day-two.js
@@ -1,25 +1,65 @@
 document.addEventListener("DOMContentLoaded", function() {
     let dayTwoString = document.getElementById("moves").textContent;
-    const dayTwoArray = dayTwoString.split("\n");
+    let dayTwoArray = dayTwoString.split("\n");
+    dayTwoArray.pop();
+    console.log(dayTwoArray[0]);
 
-    const dataTest = [
-        ["forward", "5"],
-        ["down", "5"],
-        ["forward", "8"],
-        ["up", "3"],
-        ["down", "8"],
-        ["forward", "2"],
-    ];
+    let newArray = [];
 
-    const commandsDirections = [];
-
-    for (let i = 0; i < dayTwoArray.length; i++) {
-        commandsDirections.push(dayTwoArray[i].split(" ", 2));
+    for (let command of dayTwoArray) {
+        let newCommand = command.split(" ");
+        newArray.push(newCommand);
     }
 
-    getTheCommands(commandsDirections);
-});
+    generateNewArrar(newArray);
+    getNewDirections(newArray);
 
+    //varible test oart 1
+
+    //varible test part 2
+});
+/**
+ * Will runn on the array and read the directions
+ * printing the final result for the data
+ * also doung the multiplication to the answer of the challenge
+ *
+ */
+function getNewDirections(directions) {
+    let horizontalPosition = 0;
+    let aimTotal = 0;
+    let depthTotal = 0;
+
+    for (let i = 0; i < directions.length; i++) {
+        if (directions[i][0] === "forward") {
+            depthTotal = depthTotal + directions[i][1] * aimTotal;
+            horizontalPosition += directions[i][1];
+        } else if (directions[i][0] === "down") {
+            aimTotal += directions[i][1];
+        } else {
+            aimTotal = aimTotal - directions[i][1];
+        }
+    }
+
+    console.log("horizontal positon", horizontalPosition);
+    console.log("aim total", aimTotal);
+    console.log("depth total", depthTotal);
+    console.log("muiltiply result:", horizontalPosition * depthTotal);
+}
+
+/**
+ * Second Part of the Challenge
+ * WIll change the string number to
+ * a number number
+ */
+function generateNewArrar(directions) {
+    for (let i = 0; i < directions.length; i++) {
+        directions[i][1] = parseInt(directions[i][1]);
+    }
+}
+
+/**
+ * Fir the first part of the challenge
+ */
 function getTheCommands(directions) {
     let forwardCommand = 0;
     let upCommand = 0;
